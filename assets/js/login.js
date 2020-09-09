b93ba198e99ebd1858f02cfbdfce7e39ccf6106e
lingpeng0917 <1608869931@qq.com>
2020-09-09 21:18:34 +0800

    完成了登录和注册功能模块

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,6 +14,8 @@ $(function() {
     //自定义表单验证规则
     //通过layui获取form
     var form = layui.form
+        // 通过layui获取layer
+    var layer = layui.layer
         //通过form.verify()函数自定义校验规则
     form.verify({
         // 校验密码的规则
@@ -29,4 +31,42 @@ $(function() {
             }
         }
     })
+
+
+    // 通过form表单监听注册事件
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功')
+            $('#link_login').click()
+        })
+    })
+
+    // 通过form表单监听登录事件
+    $('#form_login').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            type: 'post',
+            data: $(this).serialize(),
+            success: function(res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
+
+
 })
